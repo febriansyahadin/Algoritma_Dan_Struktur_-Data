ee60cd6e22afab2f8ae1162e1b52c01b654eba24
febriansyahadin <febriansyahhoho@gmail.com>
2024-03-24 10:13:36 +0700

    push

---
+++ pertemuan5/BruteForceDivideConquer/Laporan.md
@@ -0,0 +1,16 @@
+Nama : Febriansyah Adi N
+Kelas : 1H
+Nim : 2341720023
+## Pertemuan 4
+## 4.2 Menghitung nilai Faktorial dengan Algoritma Brute Force dan Devide and Conquer
+![alt text](image.png)
+![alt text](image-1.png)
+## 4.2.2 Verifikasi Hasil Percobaan 
+![alt text](image-2.png)
+## 4.2.3 Pertayaan 
+1. Pada base line Algoritma Divide Conquer untuk melakukan pencarian nilai faktorial, jelaskan 
+perbedaan bagian kode pada penggunaan if dan else!
+2. Apakah memungkinkan perulangan pada method faktorialBF() dirubah selain menggunakan 
+for?Buktikan!
+3. Jelaskan perbedaan antara fakto *= i; dan int fakto = n * faktorialDC(n-1); !
+## 4.3 Menghitung Hasil Pangkat dengan Algoritma Brute Force dan Dividee and Conquer
---
+++ pertemuan5/BruteForceDivideConquer/MainSum.java
@@ -0,0 +1,43 @@
+package pertemuan5.BruteForceDivideConquer;
+import java.util.Scanner;
+
+public class MainSum {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Program Menghitung Keuntungan Total (Satuan juta; misal: 5.9)");
+
+    System.out.print("Masukkan jumlah perusahaan: ");
+    int elem = sc.nextInt();
+
+    Sum[][] sm = new Sum[elem][];
+    for (int i = 0; i < elem; i++) {
+        System.out.println("================================================================");
+        System.out.print("Masukkan jumlah bulan untuk perusahaan " + (i + 1) + ": ");
+        int elm = sc.nextInt();
+        sm[i] = new Sum[elm];
+
+        for (int j = 0; j < elm; j++) {
+            System.out.print(" > Masukkan keuntungan di bulan ke-" + (j + 1) + ": ");
+            sm[i][j] = new Sum(1);
+            sm[i][j].keuntungan[0] = sc.nextDouble();
+        }
+    }
+
+    System.out.println("================================================================");
+    for (int i = 0; i < elem; i++) {
+        System.out.println(">> PERUSAHAAN " + (i + 1));
+        double[] untung = new double[sm[i].length];
+        for (int j = 0; j < sm[i].length; j++) {
+            untung[j] = sm[i][j].keuntungan[0]; 
+        }
+        System.out.println("== Brute Force ==");
+        System.out.println("Total keuntungan perusahaan selama " + sm[i].length + " bulan   : "
+                + new Sum(sm[i].length).totalBF(untung));
+                System.out.println("== Divide Conquer ==");
+        System.out.println("Total keuntungan perusahaan selama " + sm[i].length + " bulan   : "
+                + new Sum(sm[i].length).totalDC(untung, 0, sm[i].length - 1));
+        System.out.println();
+
+}
+    }
+}
